ef366e1fbe1c626d27ac756027cca88b636aae12
Stente3 <stentem0103@gmail.com>
2024-11-30 15:58:44 -0500

    Se agregó más documentación dentro del código

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ const port = 5555;
 // Middleware para parsear el body de las peticiones
 app.use(express.json());
 
-// Usar rutas
+/* Usar rutas */
+// ruta relacionada con usuarios
 app.use("/usuarios", usuariosRoutes);
+// ruta relacionada con cursos
 app.use("/cursos", cursosRoutes);
+// ruta relacionada con categorias
 app.use("/categorias", categoriasRoutes);
 
+// Iniciamos el servidor
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
